1a570c46ccc5417314f487e8a4daa67a930f3268
Artur Owczarek <artur@owczarek.info>
2017-02-27 18:39:41 +0100

    Complete lesson 18

--- app/Comment.php
+++ app/Comment.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['body', 'post_id'];
+
     public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -20,4 +20,9 @@ class Post extends Model
 
         $this->comments()->create(compact('body'));
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
