617e61d842671ef738af272d71c6cb54daf78385
Artur Owczarek <owczarek.artur@gmail.com>
2017-02-28 13:36:48 +0100

    Fix problem with bcrypting user password

--- app/User.php
+++ app/User.php
@@ -32,6 +32,11 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function publish(Post $post)
     {
         $this->posts()->save($post);
